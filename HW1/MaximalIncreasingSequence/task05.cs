6569ca976e175845252ee81f2d5adc41d90875f4
adminLaptop <adminLaptop@PCLAPTOP>
2016-05-05 14:58:19 +0300

    up

---
+++ HW1/MaximalIncreasingSequence/task05.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalIncreasingSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int count = 1;
+            int maxLength = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] < array[i])
+                {
+                    count++;
+                    maxLength = Math.Max(maxLength, count);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            Console.WriteLine(maxLength);
+        }
+    }
+}
---
+++ HW1/PrimeNumbers/task15.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class task15
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+
+        }
+    }
+}
