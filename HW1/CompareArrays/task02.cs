b97d91cff4f2f5b408c85342433d95e8799b92d1
ilieviv <iliev.ivaylo.p@gmail.com>
2016-05-05 13:44:58 +0300

    up

---
+++ HW1/CompareArrays/task02.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareArrays
+{
+    class task02
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array1 = new int[n];
+            int[] array2 = new int[n];
+            bool equal = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                array2[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    equal = false;
+                }
+            }
+
+            if (equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+        }
+    }
+}
---
+++ HW1/CompareCharArrays/task03.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareCharArrays
+{
+    class task03
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            int compare = first.CompareTo(second);
+
+            if (compare == -1)
+            {
+                Console.WriteLine("<");
+            }
+            else if (compare == 1)
+            {
+                Console.WriteLine(">");
+            }
+            else if (compare == 0)
+            {
+                Console.WriteLine("=");
+            }
+        }
+    }
+}
