b10fd3c39cb9ba18dfb3a2a4b88fd25791156e3b
ilieviv <iliev.ivaylo.p@gmail.com>
2016-05-12 16:09:36 +0300

    upload

---
+++ HW3/SayHello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, {0}!", Name());
+        }
+
+        public static string Name()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
