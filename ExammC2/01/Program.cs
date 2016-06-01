fd830e438d81bdf22ea9d89907ad1c335da7ac6a
ilieviv <iliev.ivaylo.p@gmail.com>
2016-06-01 22:19:13 +0300

    exam

---
+++ ExammC2/01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            var operation = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            BigInteger sum = 0;
+            string result = "";
+
+            BigInteger resultFirst = 0;
+            foreach (char digit in first)
+            {
+                resultFirst = (digit - 'a') + resultFirst * 26;
+            }
+
+
+            BigInteger resultSecond = 0;
+            for (int i = 0; i < second.Length; i++)
+            {
+                resultSecond += BigInteger.Pow(7, i) * BigInteger.Parse(second[second.Length - 1 - i].ToString());
+            }
+
+
+            if (operation == "+")
+            {
+                sum = resultFirst + resultSecond;
+            }
+            else if (operation == "-")
+            {
+                sum = resultFirst - resultSecond;
+            }
+
+
+
+            while (sum > 0)
+            {
+                result = (sum % 9).ToString() + result;
+                sum /= 9;
+            }
+            BigInteger res = BigInteger.Parse(result);
+            Console.WriteLine(res);
+
+        }
+    }
+}
---
+++ ExammC2/02/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToArray();
+            string[] path = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            int soul = 0;     //@ 
+            int food = 0;     //*
+            int deadlock = 0; //x
+            int counter = 0;
+
+            if (input[0] == '@')
+            {
+                soul++;
+                input[0] = 'e';
+            }
+            if (input[0] == '*')
+            {
+                food++;
+                input[0] = 'e';
+            }
+            if (input[0] == 'x')
+            {
+                Console.WriteLine("You are deadlocked, you greedy kitty!");
+                Console.WriteLine("Jumps before deadlock: {0}", counter);
+                return;
+            }
+
+            int jumps = path.Length;
+            int p = 0;
+
+            for (int i = 0; i < jumps; i++)
+            {
+
+                int move = int.Parse(path[i]) + p;                  //move =    1     0       -1      10
+                if (move < input.Length && move >= 0)
+                {
+                    p = move;                                       //p =       1     0        6      3   
+                }
+                else if (move < 0)
+                {
+                    p = (input.Length + move) % input.Length;
+                }
+                else if (move >= input.Length)
+                {
+                    p = move % input.Length;
+                }
+
+
+                if (input[p] == '@')
+                {
+                    soul++;
+                    input[p] = 'e';
+                }
+                else if (input[p] == '*')
+                {
+                    food++;
+                    input[p] = 'e';
+                }
+                else if (input[p] == 'x')
+                {
+                    if (p % 2 == 0)
+                    {
+                        input[p] = '@';      //added
+                        soul--;
+                    }
+                    else if (p % 2 == 1)
+                    {
+                        input[p] = '*';      //added
+                        food--;
+                    }
+
+                    deadlock++;
+                }
+                counter++;
+
+                if (soul < 0 && food < 0)
+                {
+                    break;
+                }
+
+            }
+
+            if (soul < 0 || food < 0)
+            {
+                Console.WriteLine("You are deadlocked, you greedy kitty!");
+                Console.WriteLine("Jumps before deadlock: {0}", counter);
+            }
+            Console.WriteLine("Coder souls collected: {0}", soul);
+            Console.WriteLine("Food collected: {0}", food);
+            Console.WriteLine("Deadlocks: {0}", deadlock);
+
+        }
+    }
+}
