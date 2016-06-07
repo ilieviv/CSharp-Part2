4d059492cca0ad48414038e69719aba023ab7163
ilieviv <PC1>
2016-06-07 09:27:43 +0300

    test sync

---
+++ LINQ/Exercises/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "  111 $ @ Ivan Ivanov %%";
+
+            char[] trimChars = new char[] { ' ', '1', '$', '@', '#', '%'};
+
+            string newtext = text.Trim(trimChars);
+
+            Console.WriteLine(newtext);
+             
+        }
+
+    }
+}
